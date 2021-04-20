2a38bb2e937c9d319ae7ab4d964783e129dbe94c
sherlie <dodoinwl@gmail.com>
2021-04-20 22:30:23 +0300

    delete js components

--- client/src/EntryDetails.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const EntryDetails = () => {
-  return(
-    <div> entry details </div>
-  )
-}
-
-export default EntryDetails;
--- client/src/EntryRow.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const EntryRow = () => {
-  return(
-    <div> entry row </div>
-  )
-}
-
-export default EntryRow;
